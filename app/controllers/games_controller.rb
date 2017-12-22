60413d5ec215cccf5badf412e9138fa0840d81cb
Ursula Moreno-VanderLaan <uvanderlaan@gmail.com>
2017-12-22 04:30:59 +0000

    Installed Factory bot, fleshed out game controller, added html pages for games, put in helper text/comments for later work, wrote a few tests for games, created static pages controller, created index page, wrote factory for users, began game factory write, created routes for games. i think that's it.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,33 @@
 class GamesController < ApplicationController
 	before_action :authenticate_user!
-
 	
-	def index
+	def new 
+		@game = Game.new
+	end
+	
+	def create
+# I suspect I'm missing significant chunks of data we'll want later in the game_create_params section here
+		@game = current_user.games.create(game_create_params)
+		if @game.valid?
+			redirect_to game_path(@game)
+		else
+			flash[:alert] = "Couldn't Create Game!"
+			render :new, :status => :unprocessable_entity
+		end
 	end
 
 	def show
+		@game = Game.find(params[:id])
+	end
+	
+	private
+	
+	def current_game
+		@current_game ||= Game.find(params[:id])
+	end
+	
+	def game_create_params
+		params.require(:game).permit(:name)
 	end
 
 end
