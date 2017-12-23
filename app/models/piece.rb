594d7a15b11ff64312baac0a9aa0a9547a6179f3
Kris Camp <kris.camp@outlook.com>
2017-12-23 13:16:13 +0000

    add method for vertical obstruct for piece model

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -7,9 +7,21 @@ class Piece < ApplicationRecord
         return true if occupied?(x, position_y)
       end
     elsif position_x > end_x # checks from left to right
-      (position_x - 1).upto(end_x) do |x|
+      (position_x - 1).downto(end_x) do |x|
         return true if occupied?(x, position_y)
     end
+    # false
   end
-  # false
+  
+  def vertical_obstruct?(end_x, end_y)
+    if position_y < end_y #checks vertical down
+      (position_y - 1).upto(end_y - 1) do |y|
+        return true if occupied?(position_x, y)
+      end
+    elsif position_y > end_y # checks vertical up
+      (position_y + 1).downto(end_y, + 1) do |y|
+        return true if occupied?(position_x, y)
+      end
+    end
+    #false
 end
