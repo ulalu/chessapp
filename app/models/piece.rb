30786bc17750e9c7b9f38af059baeec260516d79
Kris Camp <kris.camp@outlook.com>
2017-12-23 13:05:54 +0000

    add the horizontal obstructed method to peice class

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,2 +1,15 @@
 class Piece < ApplicationRecord
+  belongs_to :game
+  
+  def horizontal_obstruct?(end_x, end_y)
+    if position_x < end_x # checks from right to left
+      (position_x + 1).upto(end_x) do |x|
+        return true if occupied?(x, position_y)
+      end
+    elsif position_x > end_x # checks from left to right
+      (position_x - 1).upto(end_x) do |x|
+        return true if occupied?(x, position_y)
+    end
+  end
+  # false
 end
