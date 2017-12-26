84f3e644a9b7316c9350c1c7b0c335b0e2b3ad2e
Zach Johnson <bballzach64@gmail.com>
2017-12-26 19:51:43 +0000

    Schema corrections

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 20171221134612) do
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
