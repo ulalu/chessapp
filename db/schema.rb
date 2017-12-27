46a626c17fe8136831127698d5b14ab6e04733cd
Tiffany Wismer <tkwismer@gmail.com>
2017-12-27 01:23:33 +0000

    Migration

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20171226183519) do
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
-    t.string   "name"
     t.integer  "user_id"
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
