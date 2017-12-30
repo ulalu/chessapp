682943f2d783da1417cfcd2a3c3a2d87b8dc7f8e
Kris Camp <kris.camp@outlook.com>
2017-12-30 14:58:42 +0000

    added a method to examine path to determine what direction possible move might go

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,40 +1,41 @@
 class Piece < ApplicationRecord
 
   belongs_to :game
-
-  def occupied?(x, y)
-    game.pieces.where(position_x: x, position_y: y).present?
+  
+  def examine_path(position_x, position_y, end_x, end_y)
+    if position_y == end_y
+      'horizontal'
+    elsif position_x == end_x
+      'vertical'
+    elsif (end_y - position_y).abs == (end_x - position_x).abs
+      'diagonal'
+    end
   end
 
-  def obstructed?(end_x, end_y)
-    horizontal_obstruct?(end_x)
-    vertical_obstruct?(end_y)
-    diagonal_obstruct?(end_x, end_y)
-  end
 
   # Checks btw piece and desired position for obstruction on horizontal.
   def horizontal_obstruct?(end_x)
     if position_x < end_x # checks from right to left
-      (position_x + 1).upto(end_x) do |x|
-        return true if occupied?(x, position_y)
+      (position_x + 1).upto(end_x - 1) do |x|
+        return true if square_occupied?(x, position_y)
       end
     elsif position_x > end_x # checks from left to right
-      (position_x - 1).downto(end_x) do |x|
-        return true if occupied?(x, position_y)
+      (position_x - 1).downto(end_x + 1) do |x|
+        return true if square_occupied?(x, position_y)
       end
-      false
     end
+    false
   end
   
   # Checks btw piece and desired position for obstruction on vertical.
   def vertical_obstruct?(end_y)
     if position_y < end_y # checks vertical down
-      (position_y - 1).upto(end_y - 1) do |y|
-        return true if occupied?(position_x, y)
+      (position_y + 1).upto(end_y - 1) do |y|
+        return true if square_occupied?(position_x, y)
       end
     elsif position_y > end_y # checks vertical up
-      (position_y + 1).downto(end_y, + 1) do |y|
-        return true if occupied?(position_x, y)
+      (position_y - 1).downto(end_y + 1) do |y|
+        return true if square_occupied?(position_x, y)
       end
     end
     false
@@ -45,16 +46,44 @@ class Piece < ApplicationRecord
       (position_x + 1).upto(end_x - 1) do |x|
         diag_y = x - position_x
         y = end_y > position_y ? position_y + diag_y : position_y - diag_y
-        return true if occupied?(x, y)
+        return true if square_occupied?(x, y)
       end
     elsif position_x > end_x # checks diagonal and up
       (position_x - 1).downto(end_x + 1) do |x|
         diag_y = position_x - x
         y = end_y > position_y ? position_y + diag_y : position_y - diag_y
-        return true if occupied?(x, y)
+        return true if square_occupied?(x, y)
       end
     end
     false
   end
-end # end of class
+  
+  def square_occupied?(x, y)
+    game.pieces.where(position_x: x, position_y: y).present?
+  end
+  
+  def examine_path(position_x, position_y, end_x, end_y)
+    if position_y == end_y
+      'horizontal'
+    elsif position_x == end_x
+      'vertical'
+    elsif (end_y - position_y).abs == (end_x - position_x).abs
+      'diagonal'
+    end
+  end
 
+  def obstructed?(x, y)
+    path = examine_path(position_x, position_y, x, y)
+    
+    if path
+      return horizontal_obstruct?(x) if path == 'horizontal'
+  
+      return vertical_obstruct?(y) if path == 'vertical'
+  
+      return diagonal_obstruct?(x, y) if path == 'diagonal'
+    else
+      return false
+    end
+  end
+  
+end # end of class
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  describe 'is_obstructed? method' do
-    it "should return true if left is obstructed" do
+  describe 'obstructed? method' do
+    it "should return true if right is obstructed" do
       user = FactoryBot.create(:user)
-      game = FactoryBot.create(:game)
-      piece = FactoryBot.create(:piece, position_x: 2, position_y: 0)
-      obstruction = FactoryBot.create(:piece, position_x: 1, position_y: 0)
+      game = FactoryBot.create(:game, user: user)
+      piece = FactoryBot.create(:piece, position_x: 0, position_y: 0, game: game)
+      obstruction = FactoryBot.create(:piece, position_x: 2, position_y: 0, game: game)
       
-      expect(piece.is_obstructed?(0, 0)).to eq(true)
+      expect(piece.obstructed?(5, 0)).to eq(true)
     end
   end
 end
