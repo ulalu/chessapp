168c17be049d133cb39267ddd4d35770577eedd8
Tiffany Wismer <tkwismer@gmail.com>
2018-01-05 04:10:11 +0000

    Pieces at positions

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,44 +4,51 @@ class Game < ApplicationRecord
   has_many :pieces
 
   validates :name, presence: true
+  after_create :populate_board, :piece_at_location
 	
 	def populate_board
   	# Populates white pieces in the database
     (0..7).each do |p|
       Pawn.create(game_id: id, type: 'Pawn', color: 'white', position_x: p, position_y: 1)
-  end
+  	end
     
-  Rook.create(game_id: id, type: 'Rook', color:'white', position_x: 0, position_y: 0)
-  Rook.create(game_id: id, type: 'Rook', color:'white', position_x: 7, position_y: 0)
+	  Rook.create(game_id: id, type: 'Rook', color:'white', position_x: 0, position_y: 0)
+	  Rook.create(game_id: id, type: 'Rook', color:'white', position_x: 7, position_y: 0)
 
-  Knight.create(game_id: id, type: 'Knight', color: 'white', position_x: 1, position_y: 0)
-  Knight.create(game_id: id, type: 'Knight', color: 'white', position_x: 5, position_y: 0)
-
-
-  Bishop.create(game_id: id, type: 'Bishop', color: 'white', position_x: 2, position_y: 0)
-  Bishop.create(game_id: id, type: 'Bishop', color: 'white', position_x: 6, position_y: 0)
-
-  Queen.create(game_id: id, type: 'Queen', color: 'white', position_x: 3, position_y: 0)
-  King.create(game_id: id, type: 'King', color: 'white', position_x: 4, position_y: 0)
+	  Knight.create(game_id: id, type: 'Knight', color: 'white', position_x: 1, position_y: 0)
+	  Knight.create(game_id: id, type: 'Knight', color: 'white', position_x: 5, position_y: 0)
 
 
-  # Populates black pieces in the database
-  (0..7).each do |p|
-    Pawn.create(game_id: id, type: 'Pawn', color: 'black', position_x: p, position_y: 6)
-  end
+	  Bishop.create(game_id: id, type: 'Bishop', color: 'white', position_x: 2, position_y: 0)
+	  Bishop.create(game_id: id, type: 'Bishop', color: 'white', position_x: 6, position_y: 0)
 
-  Rook.create(game_id: id, type: 'Rook', color:'black', position_x: 0, position_y: 7)
-  Rook.create(game_id: id, type: 'Rook', color:'black', position_x: 7, position_y: 7)
-
-  Knight.create(game_id: id, type: 'Knight', color: 'black', position_x: 1, position_y: 7)
-  Knight.create(game_id: id, type: 'Knight', color: 'black', position_x: 6, position_y: 7)
+	  Queen.create(game_id: id, type: 'Queen', color: 'white', position_x: 3, position_y: 0)
+	  King.create(game_id: id, type: 'King', color: 'white', position_x: 4, position_y: 0)
 
 
-  Bishop.create(game_id: id, type: 'Bishop', color: 'black', position_x: 2, position_y: 7)
-  Bishop.create(game_id: id, type: 'Bishop', color: 'black', position_x: 5, position_y: 7)
-    
-  Queen.create(game_id: id, type: 'Queen', color: 'black', position_x: 3, position_y: 7)
-  King.create(game_id: id, type: 'King', color: 'black', position_x: 4, position_y: 7)
-  end
+	  # Populates black pieces in the database
+	  (0..7).each do |p|
+	    Pawn.create(game_id: id, type: 'Pawn', color: 'black', position_x: p, position_y: 6)
+	  end
+
+	  Rook.create(game_id: id, type: 'Rook', color:'black', position_x: 0, position_y: 7)
+	  Rook.create(game_id: id, type: 'Rook', color:'black', position_x: 7, position_y: 7)
+
+	  Knight.create(game_id: id, type: 'Knight', color: 'black', position_x: 1, position_y: 7)
+	  Knight.create(game_id: id, type: 'Knight', color: 'black', position_x: 6, position_y: 7)
+
+
+	  Bishop.create(game_id: id, type: 'Bishop', color: 'black', position_x: 2, position_y: 7)
+	  Bishop.create(game_id: id, type: 'Bishop', color: 'black', position_x: 5, position_y: 7)
+	    
+	  Queen.create(game_id: id, type: 'Queen', color: 'black', position_x: 3, position_y: 7)
+	  King.create(game_id: id, type: 'King', color: 'black', position_x: 4, position_y: 7)
+	  end
+
+  def piece_at_location(position_x, position_y)
+		piece = Piece.where(position_x: x_position, position_y: y_position, game_id: self.id).first
+	end
+
+
 end
 
