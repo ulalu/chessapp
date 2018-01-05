4c702323a6d641b352dba91f65ffca26e9ec0a45
Ursula Moreno-VanderLaan <uvanderlaan@gmail.com>
2018-01-05 04:53:38 +0000

    first working piece move test

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe King, type: :model do
-        
-        it "should be able to move one square diagonally" do
-            game = FactoryBot.create(:game)
-            king = FactoryBot.create(:king, position_x: 5, position_y: 0)
+        describe 'legal?' do
             
-            diagonal = king.legal?(4, 1)
-            expect(diagonal).to be(true)
-        end
+            it "should be able to move one square diagonally" do
+                user = FactoryBot.create(:user)
+                game = FactoryBot.create(:game, user: user)
+                king = FactoryBot.create(:king, position_x: 5, position_y: 0, game: game)
+                
+                expect(king.legal(4, 1)).to eq(true)
 
+            end
+        end
         
 end
