a5ccc7cfddb4d7feb9df02998ef20693cd7ef468
Kris Camp <kris.camp@outlook.com>
2018-01-06 16:08:31 +0000

    added test to make sure game is successfully created in db

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,22 +2,28 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
     describe "games#index action" do
-        it "should show the new game page" do
+        it "should successfully allow user to access new page" do
             user = FactoryBot.create(:user)
             sign_in user
             get :new
+            
             expect(response).to have_http_status(:success)
         end
         
-        it "should show the create game page" do
-            get :create
-            expect(response).to have_http_status(:success)
+        it "should redirect to game show if game create successful" do
+            user = FactoryBot.create(:user)
+            sign_in user
+            
+            post :create, params: {
+              game: { 
+                name: 'grandmaster', 
+                user: user 
+              }
+            }
+            game = Game.last
+            expect(response).to redirect_to(game_path(game))
         end
         
-        it "should show the game" do
-            get :show
-            expect(response).to have_http_status(:success)
-        end
     end
     
 end
