00fc44bb145c2bc5dcd591971821fecc3c7e6b8d
Kris Camp <kris.camp@outlook.com>
2018-01-06 15:37:45 +0000

    corrected test for allow user to access game page

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe GamesController, type: :controller do
     describe "games#index action" do
         it "should show the new game page" do
+            user = FactoryBot.create(:user)
+            sign_in user
             get :new
             expect(response).to have_http_status(:success)
         end
