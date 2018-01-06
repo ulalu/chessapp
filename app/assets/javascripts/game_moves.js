6a9b3a99729fb4f425699045cdbe98d4e0b1ca8b
Unknown <terry@lowerytech.net>
2018-01-06 12:34:46 -0600

    Retrive source of moves
    - Added td id to identify row and col
    - Console output of source row and col

--- app/assets/javascripts/game_moves.js
+++ app/assets/javascripts/game_moves.js
@@ -1,9 +1,25 @@
 $(function () {
 	$('.draggable').draggable();
+
+	var fromRow = -1;
+	var fromCol = -1;
+
+	// $('.draggable').click(function () {
+	// 	td = $(this).parent().get(0);
+	// 	sourceMove = td.id;
+	// 	console.log(sourceMove);
+	//
+	// })
 	$('.draggable').on('mousedown', function (data) {
-		console.log(data);
+		td = $(this).parent().get(0);
+		sourceMove = td.id;
+
+		fromRow = sourceMove.split("_")[1];
+		fromCol = sourceMove.split("_")[3];
+
 	});
 	$('.draggable').on('mouseup', function (data) {
-		console.log(data);
+		//console.log(data);
+		console.log("Moved piece from row: " + fromRow + " col: " + fromCol);
 	})
 });
