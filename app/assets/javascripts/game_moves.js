b19b8f2f6684f8d6fd9ea06acb5ec87957ba08dd
Unknown <terry@lowerytech.net>
2018-01-07 01:20:44 -0600

    WIP - game_moves
    - Wrote function to get all TD elements and store in array
    - Wrote function to get all TD elements from all TD element array and return the position array which stores height, width, top, left, and td id's
    - All output is going to console.log at this time.
    - Updated games.scss to remove padding from TD elements to make height and width consistent with top and left numbers
    - Updated show.html.erb to include id in td elements without pieces.

--- app/assets/javascripts/game_moves.js
+++ app/assets/javascripts/game_moves.js
@@ -1,6 +1,51 @@
+// Retreive all TD elements and return as an array
+function getAlltableDataElements() {
+	tdArray = $("td");
+	return tdArray;
+}
+function tdDataStruct(name, top, left) {
+	this.name = name;
+	this.top = top;
+	this.left = left;
+}
+// Build a TD element array by search each items in tdElements. This is to store positions of these items
+function tdLocations(tdEleArray) {
+	positionArray = [];
+	heightWidthSelector = 'td#row_0_col_0';
+	// Store height and width of bpx
+	console.log($('td#row_0_col_0').height());
+	positionArray.push($(heightWidthSelector).height());
+	positionArray.push($(heightWidthSelector).width());
+
+	for(i = 0; i < tdEleArray.length; i++) {
+		console.log($(tdEleArray[i]).position().top);
+		t = $(tdEleArray[i]).position().top;
+		left = $(tdEleArray[i]).position().left;
+		name = $(tdEleArray[i]).attr('id');
+
+		console.log("Top: " + t + " Left: " + left + " Name: " + name );
+		datastruct = new tdDataStruct(name, t, left);
+		positionArray.push(datastruct);
+
+
+	}
+
+	console.log(positionArray);
+	return positionArray;
+
+}
+
+// All TD elements store here
+var tdElements = null;
 $(function () {
+	tdElements = getAlltableDataElements();
+	console.log(tdElements);
+
+	var blah = tdLocations(tdElements);
+
 	$('.draggable').draggable();
 
+	// Variables to keep track of source row and col from piece selected
 	var fromRow = -1;
 	var fromCol = -1;
 
