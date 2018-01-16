9d729b3d8ea4edbcbb917a10b84b572f8c27e92a
Kris Camp <kris.camp@outlook.com>
2018-01-16 23:36:48 +0000

    Removed state column from pieces

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115233440) do
+ActiveRecord::Schema.define(version: 20180116233430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20180115233440) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "game_id"
-    t.string   "state"
     t.index ["game_id"], name: "index_pieces_on_game_id", using: :btree
   end
 
