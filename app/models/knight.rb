33bf79af477986429e2a011f8dc58cc61517f1bf
Ula MV <uvanderlaan@gmail.com>
2018-01-17 11:17:04 -0700

    just to keep piece models consistent

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,11 +1,11 @@
 class Knight < Piece
   
-  def valid_move?(end_x, end_y)
-    y_diff = (position_y - end_y).abs
-    x_diff = (position_x - end_x).abs
+  def valid_move?(x, y)
+    y_diff = (position_y - y).abs
+    x_diff = (position_x - x).abs
     
     
-    return false if is_same_color?(end_x, end_y)
+    return false if is_same_color?(x, y)
     (x_diff) == 2 && (y_diff == 1) ? true : false
     (y_diff) == 2 && (x_diff == 1) ? true : false
     
